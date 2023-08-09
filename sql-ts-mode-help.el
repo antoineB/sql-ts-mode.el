2aa0cf3b4e67508a372afc008ca1ee4a4420065a
antoineB <antoine597@gmail.com>
2023-08-09 23:52:14 +0200

    *

--- sql-ts-mode-help.el
+++ sql-ts-mode-help.el
@@ -2,12 +2,33 @@
 (treesit-ready-p 'sql)
 (treesit-parser-create 'sql)
 
+;; aller sur schema.sql
+(setq test-data (sql-pg-ts-mode--extract-db-from-schema (current-buffer)))
+(setq sql-pg-ts-mode--datastore
+      (let* ((db (sql-pg-ts-mode-make-database "test"))
+             (schema-db (gethash "test" db))
+             (schema (gethash "public" schema-db)))
+        (puthash "public"
+                 `((tables . ,test-data)
+                   (functions . ,(cdr (assoc 'functions schema))))
+                 schema-db)
+        db))
+(defun sql-pg-ts-mode-current-database-name ()
+  "test")
+
+
+;; (49 52 (("password_recovery_token" ("used_at" "used_at" "password_recovery_token.used_at") ("created_at" "created_at" "password_recovery_token.created_at") ("token" "token" "password_recovery_token.token") ("profile_id" "profile_id" "password_recovery_token.profile_id"))))
+
+
 (treesit-node-at (point))
 
 (treesit-parent-until
  (treesit-node-at (point))
- (lambda (node) (equal (treesit-node-type node) "join_clause"))
+ (lambda (node) (equal (treesit-node-type node) "create_table_statement"))
  t)
+
+(pcase toto
+  ( sym))
 
 (treesit-node-top-level
  (treesit-node-at (point))
